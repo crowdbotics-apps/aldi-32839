bf75ece80d94770905dbde7760cc85a1433fddf0
Crowdbotics <team@crowdbotics.com>
2022-01-14 13:54:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "aldi_32839",
+  url: "https://aldi_32839.botics.co",
+  api: "https://aldi_32839.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
